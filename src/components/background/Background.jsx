6976d65a10d957a32fb690a75af6d3bfe3c154f1
nuel012003 <immanuel.rhs20@gmail.com>
2026-05-28 06:11:44 +0700

    fix bug

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -1,9 +1,9 @@
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import Particles from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 export default function Background() {
-  const particlesInit = async (main) => {
-    await loadFull(main);
+  const particlesInit = async (engine) => {
+    await loadSlim(engine);
   };
 
   return (
@@ -13,28 +13,22 @@ export default function Background() {
       className="absolute inset-0 -z-10"
       options={{
         fullScreen: false,
-
         background: {
           color: "#09090b",
         },
-
         particles: {
           number: {
             value: 120,
           },
-
           color: {
             value: "#ffffff",
           },
-
           opacity: {
             value: 0.7,
           },
-
           size: {
             value: { min: 1, max: 3 },
           },
-
           move: {
             enable: true,
             speed: 0.2,
